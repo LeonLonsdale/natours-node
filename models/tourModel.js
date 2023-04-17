c5b692bbad7870135b2693f4275da523819dafee
Leon Lonsdale <leon.dev@icloud.com>
2023-04-17 01:05:43 +0100

    add geolocations to tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,74 +2,104 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: [true, 'This tour name is already taken'],
-    trim: true,
-    // validate: validator.isAlpha, s
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a maximum group size'],
-  },
-  summary: {
-    type: String,
-    trim: true, // removes whitespace at the beginning and end of the string
-    required: [true, 'A tour must have a summary'],
-  },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-    max: [5, 'Rating must be less than or equal to 5'],
-    min: [1, 'Rating must be greater than or equal to 1'],
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: {
-    type: Number,
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-    enum: {
-      values: ['easy', 'medium', 'difficult'],
-      message:
-        '{VALUE} is not a valid difficulty. Please choose from: easy, medium, or difficult',
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: [true, 'This tour name is already taken'],
+      trim: true,
+      // validate: validator.isAlpha, s
     },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a maximum group size'],
+    },
+    summary: {
+      type: String,
+      trim: true, // removes whitespace at the beginning and end of the string
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      max: [5, 'Rating must be less than or equal to 5'],
+      min: [1, 'Rating must be greater than or equal to 1'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message:
+          '{VALUE} is not a valid difficulty. Please choose from: easy, medium, or difficult',
+      },
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have an image cover'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    startDates: [Date],
+    slug: String,
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+    startLocation: {
+      // Geospacial Data: GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have an image cover'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  startDates: [Date],
-  slug: String,
-  secretTour: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 // document middleware
 
