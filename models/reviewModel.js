54f52fe81bb15eabbbdfbbaaa17dfefa42fad5de
Leon Lonsdale <leon.dev@icloud.com>
2023-04-17 01:05:43 +0100

    schema static to calculate review stats on creation

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -43,6 +44,37 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   });
   next();
+});
+
+// calculating number of reviews and average rating for tours.
+// async because aggregate returns a promise.
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }, // match reviews where the tour field matches the tourId passed in as an argument.
+    },
+    {
+      $group: {
+        _id: '$tour', // group by the tour field
+        nRating: { $sum: 1 }, // add 1 for each review found
+        avgRating: { $avg: '$rating' }, // average all of the rating fields
+      },
+    },
+  ]);
+  // the resolved aggretate promise is an array of objects containing our results
+  // [{_id:... , nRating: x, avgRating: y}] - if we did this for all tours each tour would be an object in the array.
+  // persist results to the tours collection.
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+// set up a post-save middleware hook that calls on the calcAverageRatings static
+// post middleware does not get access to next().
+reviewSchema.post('save', function () {
+  // affects current review being saved
+  // this.constructor gives us access to the methods added to the model through statics / instance methods.
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
