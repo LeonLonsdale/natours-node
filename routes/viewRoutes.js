cd6c43558f6ace13ad42fba49e8f5ac7552a727d
Leon Lonsdale <leon.dev@icloud.com>
2023-04-23 15:46:28 +0100

    create placeholder cards on overview for tours

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,18 +3,7 @@ const { getOverview, getTour } = require('../controllers/viewController');
 
 const router = express.Router();
 
-router.get('/', (req, res) =>
-  res.status(200).render('base', {
-    tour: 'Banana boats',
-    location: 'Liverpool',
-    user: 'Leon',
-  })
-);
-router.get('/overview', getOverview);
-router.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
-  });
-});
+router.get('/', getOverview);
+router.get('/tour', getTour);
 
 module.exports = router;
