dbeda1217d6bffebdc4b8ed4c4d9ee7e99e5798b
Leon Lonsdale <leon.dev@icloud.com>
2023-04-24 20:59:17 +0100

    create tour details template

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // get all tour data
@@ -13,8 +14,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'All Tours',
+exports.getTour = catchAsync(async (req, res, next) => {
+  const { slug } = req.params;
+
+  const tour = await Tour.findOne({ slug: slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  console.log(tour);
+  if (!tour) return next(new AppError('Tour not found', 404));
+
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
