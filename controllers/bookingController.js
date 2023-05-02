75e0e4a2b23760da20ce5b24076cf2a744913175
LeonLonsdale <leon.dev@icloud.com>
2023-05-02 21:59:09 +0100

    implement stripe for checkout payments via api

---
+++ controllers/bookingController.js
@@ -0,0 +1,42 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // get the tour
+  if (!req.params.tourId)
+    return next(new AppError('A tour must be selected', 400));
+
+  const tour = await Tour.findById(req.params.tourId);
+
+  // create checkout session
+  const session = await stripe.checkout.sessions.create({
+    mode: 'payment',
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name}`,
+            description: tour.summary,
+            images: ['https://www.natours.dev/img/tours/tour-1-cover.jpg'],
+          },
+        },
+      },
+    ],
+  });
+  // send to client
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
---
+++ routes/bookingRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bookingController = require('../controllers/bookingController');
+const authController = require('../controllers/authController');
+
+const { getCheckoutSession } = bookingController;
+const { protect } = authController;
+
+const router = express.Router();
+
+router.get('/checkout-session/:tourId', protect, getCheckoutSession);
+
+module.exports = router;
