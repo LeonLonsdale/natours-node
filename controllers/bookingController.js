643625d1ac67537d40772a819bd2a516ddd4e5f8
LeonLonsdale <leon.dev@icloud.com>
2023-05-04 20:12:10 +0100

    attempt fix booking save to db

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -9,9 +9,10 @@ const factory = require('./handlerFactory');
 // NOTES STRIPE CHECKOUT USING WEBHOOK
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email })._id;
+  const user = await User.findOne({ email: session.customer_email });
+  const userId = user._id;
   const price = session.amount_total / 100;
-  await Booking.create({ tour, user, price });
+  await Booking.create({ tour, userId, price });
 };
 
 // signature : stripe adds header to the webhook request containing a signature for the webhook.
