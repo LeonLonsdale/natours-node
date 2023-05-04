714b13d330d2eb426374899f9bf976ec2a049be0
LeonLonsdale <leon.dev@icloud.com>
2023-05-04 12:08:34 +0100

    Update package.json

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -100,6 +101,7 @@ app.use(
   })
 );
 
+app.use(compression());
 // [Routing] ==============================================================
 
 // mount the routers
